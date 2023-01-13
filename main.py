48d6f4cca65c3860a99c5160d8d29588f999364a
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-01-12 21:32:21 -0300

    Artificial Intelligence
    Creation and training of artificial intelligence

--- main.py
+++ main.py
@@ -20,3 +20,16 @@ x = table [['TV', 'Radio', 'Jornal']]
 from sklearn.model_selection import train_test_split
 
 x_training, x_test, y_training, x_test = train_test_split(x, y)
+
+# Import the artificial intelligence
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+# Create artificial intelligence
+linear_regression_model = LinearRegression()
+decision_tree_model = RandomForestRegressor()
+
+# Train artificial intelligence
+
+linear_regression_model.fit(x_training, y_training)
+decision_tree_model.fit(x_training, y_training)
